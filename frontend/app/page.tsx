5fbe729cbdf7c602402718fed5b6d7b96af638c1
Axel Polleux <polleuxaxel@gmail.com>
2026-09-10 10:50:01 +0200

    feat: initiated "login" part to frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,19 +3,20 @@
 import "./app.css"
 
 export default function Home() {
-    const [data, setData] = useState([]);
-
-    async function fetchData() {
-        const response = await fetch("https://jsonplaceholder.typicode.com/photos/")
-        setData(await response.json());
-    }
-
-    useEffect(() => {
-        fetchData();
-    }, [])
+    // const [data, setData] = useState([]);
+    //
+    // async function fetchData() {
+    //     const response = await fetch("https://jsonplaceholder.typicode.com/photos/")
+    //     setData(await response.json());
+    // }
+    //
+    // useEffect(() => {
+    //     fetchData();
+    // }, [])
 
     return (
         <main>
+            
         </main>
     );
 }
